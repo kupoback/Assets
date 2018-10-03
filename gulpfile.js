d90540a24c2c430432842f9c87e178b630ac9538
Nick Makris <nick@freydesigngroup.com>
2018-10-03 17:45:14 -0500

    another minor tweak

--- gulpfile.js
+++ gulpfile.js
@@ -1,139 +1,139 @@
-var url                   = "https://domain.app/", // Your testing URL
-    proj_dir              = "../wp-content/themes/domain", // Give a project path
-    theme_name            = "domain", // Give a theme path if WP
-    // EXAMPLE
-    // ssl_key = '/Users/username/.valet/Certificates/domain.TLS.key',
-    ssl_key               = "/Users/nmak/.valet/Certificates/domain.app.key", // Replace with path for your SSL key
-    ssl_cert              = "/Users/nmak/.valet/Certificates/domain.app.crt", // Replace with path for your SSL key
-    gulp                  = require( "gulp" ),
-    sass                  = require( "gulp-sass" ),
-    autoprefixer          = require( "gulp-autoprefixer" ),
-    globbing              = require( "gulp-css-globbing" ),
-    sourcemaps            = require( "gulp-sourcemaps" ),
-    flatten               = require( "gulp-flatten" ),
-    rename                = require( "gulp-rename" ),
-    newer                 = require( "gulp-newer" ),
-    concat                = require( "gulp-concat" ),
-    uglify                = require( "gulp-uglify" ),
-    browserSync           = require( "browser-sync" ),
-    reload                = browserSync.reload,
-    _NPM_DIR              = "./node_modules",
-    _BS_DIR               = _NPM_DIR + "/bootstrap",
-    _BS_CSS               = _BS_DIR + "/scss/",
-    addPaths              = (...paths) => paths.reduce( (p, c) => p.concat( c ), [] ),
-    _BS_CSS_MUST_IMPORT   = [
-	    // Do not uncomment
-	    _BS_CSS + "_functions.scss",
-	    _BS_CSS + "_variables.scss",
-	    _BS_CSS + "_mixins.scss",
-	    _BS_CSS + "_reboot.scss",
-	    _BS_CSS + "_grid.scss",
-	    _BS_CSS + "_transitions.scss",
-	    _BS_CSS + "_media.scss",
-	    _BS_CSS + "_root.scss",
-	    _BS_CSS + "_images.scss",
-	    _BS_CSS + "_utilities.scss",
-	    _BS_CSS + "_print.scss",
-	    _BS_CSS + "_close.scss",
-	    _BS_CSS + "_type.scss",
-	    _BS_CSS + "_dropdown.scss",
-	    _BS_CSS + "_nav.scss",
-	    _BS_CSS + "_navbar.scss",
-    ],
-    _BS_ADDITIONAL_IMPORT = [
-	
-	    // Uncomment what you need
-	    // Common files
-	    // _BS_CSS + '_buttons.scss',
-	    // _BS_CSS + '_button-group.scss',
-	    // _BS_CSS + '_card.scss',
-	    // _BS_CSS + '_tables.scss',
-	    // _BS_CSS + '_list-group.scss',
-	    // _BS_CSS + '_modal.scss',
-	    // Dependent on each other
-	    // _BS_CSS + '_forms.scss',
-	    // _BS_CSS + '_custom-forms.scss',
-	    // _BS_CSS + '_input-group.scss',
-	    // End dependency
-	
-	    // Dependent on each other
-	    // _BS_CSS + '_popover.scss',
-	    // _BS_CSS + '_tooltip.scss',
-	    // End dependency
-	
-	    //Uncommon files
-	    // _BS_CSS + '_code.scss',
-	    // _BS_CSS + '_alert.scss',
-	    // _BS_CSS + '_badge.scss',
-	    // _BS_CSS + '_breadcrumb.scss',
-	    // _BS_CSS + '_carousel.scss',
-	    // _BS_CSS + '_jumbotron.scss',
-	    // _BS_CSS + '_pagination.scss',
-	    // _BS_CSS + '_progress.scss',
-    ],
-    _BS_JS                = _BS_DIR + "/dist/js/bootstrap.bundle.min.js",
-    _PLUGINS              = {
-	    _dataTables:    {
-		    scripts: {
-			    required: _NPM_DIR + "/datatables.net/js/jquery.dataTables.js",
-			    bs4:      _NPM_DIR + "/datatables.net-bs4/js/dataTables.bootstrap4.js"
-		    },
-		    styles:  _NPM_DIR + "/datatables.net-bs4/css/dataTables.bootstrap4.css"
-	    },
-	    _fancybox:      {
-		    scripts: _NPM_DIR + "/@fancyapps/fancybox/dist/jquery.fancybox.js",
-		    styles:  _NPM_DIR + "/@fancyapps/fancybox/dist/jquery.fancybox.css"
-	    },
-	    _owlCarousel:   {
-		    scripts: _NPM_DIR + "/owl-carousel-2/owl.carousel.js",
-		    styles:  {
-			    required:   _NPM_DIR + "/owl-carousel-2/assets/owl.carousel.css",
-			    additional: _NPM_DIR + "/owl-carousel-2/assets/owl.theme.default.css",
-		    },
-		    media:   _NPM_DIR + "/owl-carousel-2/assets/*.{jpg,png,gif}",
-	    },
-	    _scrollReveal:  {
-		    scripts: _NPM_DIR + "/scrollreveal/dist/scrollreveal.js"
-	    },
-	    _skipLinkFocus: {
-		    scripts: _NPM_DIR + "/skip-link-focus/skip-link-focus.js"
-	    }
-    };
+var url                    = "https://base.app/", // Your testing URL
+	proj_dir               = "../wp-content/themes/base", // Give a project path
+	theme_name             = "base", // Give a theme path if WP
+	// EXAMPLE
+	// ssl_key = '/Users/username/.valet/Certificates/domain.TLS.key',
+	ssl_key                = "/Users/nmak/.valet/Certificates/base.app.key", // Replace with path for your SSL key
+	ssl_cert               = "/Users/nmak/.valet/Certificates/base.app.crt", // Replace with path for your SSL key
+	gulp                   = require( "gulp" ),
+	sass                   = require( "gulp-sass" ),
+	autoprefixer           = require( "gulp-autoprefixer" ),
+	globbing               = require( "gulp-css-globbing" ),
+	sourcemaps             = require( "gulp-sourcemaps" ),
+	flatten                = require( "gulp-flatten" ),
+	rename                 = require( "gulp-rename" ),
+	newer                  = require( "gulp-newer" ),
+	concat                 = require( "gulp-concat" ),
+	uglify                 = require( "gulp-uglify" ),
+	browserSync            = require( "browser-sync" ),
+	reload                 = browserSync.reload,
+	_PROJ_CSS              = proj_dir + "/assets/css",
+	_PROJ_JS               = proj_dir + "/assets/js",
+	_PROJ_IMG              = proj_dir + "/assets/img",
+	_PROJ_PHP              = proj_dir + "/inc/compiled",
+	_NPM_DIR               = "./node_modules",
+	_BS_DIR                = _NPM_DIR + "/bootstrap",
+	_BS_CSS                = _BS_DIR + "/scss/",
+	addPaths               = (...paths) => paths.reduce( (p, c) => p.concat( c ), [] ),
+	_BS_CSS_MUST_IMPORT    = [
+		// Do not uncomment
+		_BS_CSS + "_functions.scss",
+		_BS_CSS + "_variables.scss",
+		_BS_CSS + "_mixins.scss",
+		_BS_CSS + "_reboot.scss",
+		_BS_CSS + "_grid.scss",
+		_BS_CSS + "_transitions.scss",
+		_BS_CSS + "_media.scss",
+		_BS_CSS + "_root.scss",
+		_BS_CSS + "_images.scss",
+		_BS_CSS + "_utilities.scss",
+		_BS_CSS + "_print.scss",
+		_BS_CSS + "_close.scss",
+		_BS_CSS + "_type.scss",
+		_BS_CSS + "_dropdown.scss",
+		_BS_CSS + "_nav.scss",
+		_BS_CSS + "_navbar.scss",
+	],
+	_BS_ADDITIONAL_IMPORT = [
+
+		// Uncomment what you need
+		// Common files
+		// _BS_CSS + '_buttons.scss',
+		// _BS_CSS + '_button-group.scss',
+		// _BS_CSS + '_card.scss',
+		// _BS_CSS + '_tables.scss',
+		// _BS_CSS + '_list-group.scss',
+		// _BS_CSS + '_modal.scss',
+		// Dependent on each other
+		// _BS_CSS + '_forms.scss',
+		// _BS_CSS + '_custom-forms.scss',
+		// _BS_CSS + '_input-group.scss',
+		// End dependency
+
+		// Dependent on each other
+		// _BS_CSS + '_popover.scss',
+		// _BS_CSS + '_tooltip.scss',
+		// End dependency
+
+		//Uncommon files
+		// _BS_CSS + '_code.scss',
+		// _BS_CSS + '_alert.scss',
+		// _BS_CSS + '_badge.scss',
+		// _BS_CSS + '_breadcrumb.scss',
+		// _BS_CSS + '_carousel.scss',
+		// _BS_CSS + '_jumbotron.scss',
+		// _BS_CSS + '_pagination.scss',
+		// _BS_CSS + '_progress.scss',
+	],
+	_BS_JS                 = _BS_DIR + "/dist/js/bootstrap.bundle.min.js",
+	_PLUGINS               = {
+		_dataTables:    {
+			scripts: {
+				required: _NPM_DIR +"/datatables.net/js/jquery.dataTables.js",
+				bs4: _NPM_DIR + "/datatables.net-bs4/js/dataTables.bootstrap4.js"
+			},
+			styles:  _NPM_DIR + "/datatables.net-bs4/css/dataTables.bootstrap4.css"
+		},
+		_fancybox:      {
+			scripts: _NPM_DIR + "/@fancyapps/fancybox/dist/jquery.fancybox.js",
+			styles:  _NPM_DIR + "/@fancyapps/fancybox/dist/jquery.fancybox.css"
+		},
+		_owlCarousel:   {
+			scripts: _NPM_DIR + "/owl-carousel-2/owl.carousel.js",
+			styles:  {
+				required:   _NPM_DIR + "/owl-carousel-2/assets/owl.carousel.css",
+				additional: _NPM_DIR + "/owl-carousel-2/assets/owl.theme.default.css",
+			},
+			media:   _NPM_DIR + "/owl-carousel-2/assets/*.{jpg,png,gif}",
+		},
+		_scrollReveal:  {
+			scripts: _NPM_DIR + "/scrollreveal/dist/scrollreveal.js"
+		},
+		_skipLinkFocus: {
+			scripts: _NPM_DIR + "/skip-link-focus/skip-link-focus.js"
+		}
+	};
 
 //Bootstrap CSS Imports
 gulp.task( "bootstrap:css", function () {
-	
+
 	var _BS_MIXINS = gulp
-		.src( [
-			_BS_CSS + "mixins/*",
-		] )
+		.src( [ _BS_CSS + "mixins/*" ] )
 		.pipe( gulp.dest( "./dev/sass/vendor/bs/mixins" ) );
-	
+
 	var _BS_UTIL = gulp
-		.src( [
-			_BS_CSS + "utilities/*",
-		] )
+		.src( [ _BS_CSS + "utilities/*" ] )
 		.pipe( gulp.dest( "./dev/sass/vendor/bs/utilities" ) );
-	
+
 	var _BS_SCSS_FILES = gulp
 		.src( addPaths( _BS_CSS_MUST_IMPORT, _BS_ADDITIONAL_IMPORT ) )
 		.pipe( gulp.dest( "./dev/sass/vendor/bs" ) );
-	
+
 	return _BS_MIXINS + _BS_UTIL + _BS_SCSS_FILES;
-	
+
 } );
 
 //Import Plugin CSS
 gulp.task( "plugin:css", function () {
-	
-	var owlAssets = "";
+
+	var owlAssets = '';
 	var _styles = [
-		// _PLUGINS._dataTables.styles,
+		_PLUGINS._dataTables.styles,
 		_PLUGINS._fancybox.styles,
 		_PLUGINS._owlCarousel.styles.required,
 		_PLUGINS._owlCarousel.styles.additional
 	];
-	
+
 	// COMMENT OUT WHAT YOU DON'T NEED
 	var _PLUGIN_STYLES = gulp
 		.src( addPaths( _styles ) )
@@ -143,26 +143,25 @@ gulp.task( "plugin:css", function () {
 			extname: ".scss"
 		} ) )
 		.pipe( gulp.dest( "./dev/sass/vendor/plugins" ) );
-	
-	if ( _styles.includes( _PLUGINS._owlCarousel.styles.required ) )
+
+	if ( _styles.includes(_PLUGINS._owlCarousel.styles.required ) )
 		owlAssets = gulp
-			.src( addPaths(
+			.src(addPaths(
 				_PLUGINS._owlCarousel.media
-			) )
-			.pipe( gulp.dest( proj_dir + "/assets/img" ) );
-	
+			))
+			.pipe(gulp.dest(_PROJ_IMG));
+
 	return _PLUGIN_STYLES + owlAssets;
-	
+
 } );
 
 //Vendor CSS
 gulp.task( "vendor:css", function () {
 	return gulp
-		.src( "dev/sass/application-vendor.scss" )
+		.src( ["./dev/sass/application-vendor.scss"] )
 		.pipe( flatten() )
-		.pipe( newer( "dev/sass/vendor/*" ) )
+		.pipe( newer( "./dev/sass/vendor/**/*" ) )
 		.pipe( sourcemaps.init() )
-		.pipe( sourcemaps.identityMap() )
 		.pipe( globbing( {
 			extensions: ".scss"
 		} ) )
@@ -177,21 +176,20 @@ gulp.task( "vendor:css", function () {
 			remove:   false,
 		} ) )
 		.pipe( rename( {
-			suffix: ".min",
+			suffix:  ".min",
 		} ) )
-		.pipe( sourcemaps.write( "./" ) )
+		.pipe( sourcemaps.write( ) )
 		.on( "error", handleError )
-		.pipe( gulp.dest( proj_dir + "/assets/css" ) );
+		.pipe( gulp.dest( _PROJ_CSS ) );
 } );
 
 // Default CSS
 gulp.task( "application:css", function () {
 	return gulp
-		.src( "dev/sass/application.scss" )
+		.src( ["dev/sass/application.scss"], {base: 'src'} )
 		.pipe( flatten() )
 		.pipe( newer( "dev/sass/**/*" ) )
 		.pipe( sourcemaps.init() )
-		.pipe( sourcemaps.identityMap() )
 		.pipe( globbing( {
 			extensions: ".scss"
 		} ) )
@@ -206,17 +204,17 @@ gulp.task( "application:css", function () {
 			remove:   false,
 		} ) )
 		.pipe( rename( {
-			suffix: ".min",
+			suffix:  ".min",
 		} ) )
-		.pipe( sourcemaps.write( "./" ) )
+		.pipe( sourcemaps.write('./') )
 		.on( "error", handleError )
-		.pipe( gulp.dest( proj_dir + "/assets/css" ) )
+		.pipe( gulp.dest( _PROJ_CSS ) )
 		;
 } );
 
 // Impost Plugin JS
 gulp.task( "plugin:js", function () {
-	
+
 	// COMMENT OUT WHAT YOU DON'T NEED
 	var _PLUGIN_SCRIPTS = [
 		_PLUGINS._dataTables.scripts.required,
@@ -226,17 +224,17 @@ gulp.task( "plugin:js", function () {
 		_PLUGINS._scrollReveal.scripts,
 		_PLUGINS._skipLinkFocus.scripts
 	];
-	
+
 	var COPY_PLUGIN_SCRIPTS = gulp
 		.src( addPaths( _PLUGIN_SCRIPTS ) )
 		.pipe( gulp.dest( "./dev/js/core/plugins" ) );
-	
+
 	var COPY_BS_SCRIPTS = gulp
 		.src( addPaths( _BS_JS ) )
 		.pipe( gulp.dest( "./dev/js/core/bs" ) );
-	
+
 	return COPY_BS_SCRIPTS + COPY_PLUGIN_SCRIPTS;
-	
+
 } );
 
 // Compile vendor scripts
@@ -248,11 +246,11 @@ gulp.task( "vendor:js", function () {
 		.pipe( concat( "application-vendor.js" ) )
 		.pipe( uglify() )
 		.on( "error", handleError )
-		.pipe( rename( {
-			suffix: ".min"
-		} ) )
-		.pipe( sourcemaps.write( "./" ) )
-		.pipe( gulp.dest( proj_dir + "/assets/js" ) );
+		.pipe(rename({
+			suffix: '.min'
+		}))
+		.pipe( sourcemaps.write('./') )
+		.pipe( gulp.dest( _PROJ_JS ) );
 } );
 
 // Compile custom Scripts
@@ -263,12 +261,12 @@ gulp.task( "application:js", function () {
 		.pipe( sourcemaps.identityMap() )
 		.pipe( concat( "application.js" ) )
 		.pipe( uglify() )
-		.pipe( rename( {
-			suffix: ".min"
-		} ) )
-		.pipe( sourcemaps.write( "./" ) )
+		.pipe(rename({
+			suffix: '.min'
+		}))
+		.pipe( sourcemaps.write('./') )
 		.on( "error", handleError )
-		.pipe( gulp.dest( proj_dir + "/assets/js" ) );
+		.pipe( gulp.dest( _PROJ_JS ) );
 } );
 
 // Theme Functions
@@ -279,7 +277,7 @@ gulp.task( "wp:theme:functions", function () {
 			newLine: "\n?>\n"
 		} ) )
 		.on( "error", handleError )
-		.pipe( gulp.dest( proj_dir + "/inc/compiled/theme/" ) );
+		.pipe( gulp.dest( _PROJ_PHP + "/theme/" ) );
 } );
 
 // Field Groups
@@ -290,7 +288,7 @@ gulp.task( "wp:theme:field_groups", function () {
 			newLine: "\n?>\n"
 		} ) )
 		.on( "error", handleError )
-		.pipe( gulp.dest( proj_dir + "/inc/compiled/theme/" ) );
+		.pipe( gulp.dest( _PROJ_PHP + "/theme/" ) );
 } );
 
 // Custom Functions
@@ -301,7 +299,7 @@ gulp.task( "wp:custom", function () {
 			newLine: "\n?>\n"
 		} ) )
 		.on( "error", handleError )
-		.pipe( gulp.dest( proj_dir + "/inc/compiled/custom/" ) );
+		.pipe( gulp.dest( _PROJ_PHP + "/custom/" ) );
 } );
 
 // Custom Post Type
@@ -312,35 +310,37 @@ gulp.task( "wp:custom:posts", function () {
 			newLine: "\n?>\n"
 		} ) )
 		.on( "error", handleError )
-		.pipe( gulp.dest( proj_dir + "/inc/compiled/custom/" ) );
+		.pipe( gulp.dest( _PROJ_PHP + "/custom/" ) );
 } );
 
+// Open the port and browser
 gulp.task( "project:open", function () {
-	
+
 	var files = [
 		"../**/*.php",
 		"../**/*.{png,jpg,gif}"
 	];
-	
+
 	var https_params = false;
-	
+
 	if ( ssl_key.length > 0 ) {
 		https_params = {
 			key:  ssl_key,
 			cert: ssl_cert
 		};
 	}
-	
+
 	browserSync.init( files, {
 		proxy:         url,
 		injectChanges: true,
 		https:         https_params
 	} );
-	
+
 } );
 
+// File Watcher
 gulp.task( "project:watcher", function () {
-	
+
 	gulp
 		.watch( "dev/sass/vendor/**/*.scss", [ "vendor:css" ] );
 	gulp
@@ -349,21 +349,22 @@ gulp.task( "project:watcher", function () {
 		.watch( "dev/js/core/**/*.js", [ "vendor:js" ] );
 	gulp
 		.watch( [ "dev/js/**/*.js", "!dev/js/core/*.js" ], [ "application:js" ] );
-	
+
 	// PHP Functions
 	gulp.watch( proj_dir + "/inc/core/**/*.php", [ "wp:theme:functions", "wp:theme:field_groups" ] );
 	gulp.watch( [ proj_dir + "/inc/custom/**/*.php" ], [ "wp:custom" ] );
 	gulp.watch( [ proj_dir + "/inc/cpt/**/*.php" ], [ "wp:custom:posts" ] );
-	
-	gulp.watch( [ proj_dir + "/assets/js/*.js", proj_dir + "/assets/css/*.css" ] ).on( "change", reload );
-	gulp.watch( [ proj_dir + "/inc/compiled/*.php", ] ).on( "change", reload );
-	
+
+	gulp.watch( [ _PROJ_JS + "/*.js", _PROJ_CSS + "/*.css" ] ).on("change", reload);
+	gulp.watch( [ _PROJ_PHP + "/*.php", ] ).on( "change", reload );
+
 } );
 
+// Set when to reload the page
 gulp.task( "project:reload", [ "vendor:css", "application:css", "vendor:js", "application:js" ], function () {
-	
+
 	browserSync.reload();
-	
+
 } );
 
 // Error reporting function
