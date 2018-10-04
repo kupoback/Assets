2552600a9f003fac69fc0c94a7120f38335ea1d7
Nick Makris <nick@freydesigngroup.com>
2018-10-04 09:39:12 -0500

    Adjusted the default necessities for a new build in default js and scss markup

--- dev/js/scripts/global.js
+++ dev/js/scripts/global.js
@@ -2,19 +2,4 @@
 
   "use strict";
 
-  if ( typeof DataTable !== 'undefined' ) {
-	  // Controls the custom Data Table created
-	  $( '[data-table]' ).DataTable( {
-		  "paging":    false,
-		  "info":      false,
-		  "searching": false,
-	  } );
-  }
-
-
-  $(".collapse").on('show.bs.collapse', function(e) {
-    if ($(this).is(e.target))
-      $(this).parent().parent().find('.collapse.show').collapse('hide');
-  });
-
 })(jQuery);
